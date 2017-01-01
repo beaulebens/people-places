1cc8bb1b64c010e1ab1439580243fa0c1401cf92
Beau Lebens <beau@dentedreality.com.au>
2016-12-31 20:10:44 -0700

    Plumbing for the People/Places plugin - Helper for adding taxonomy meta (UI, handling, etc) - Registers both taxonomies (people and places) - Adds some meta fields to each, for keeping track of things across networks - Adds filters for both people and places to the post listing screen - Adds some helpers for getting specific pieces of data from people in particular (full URLs to social profiles, gravatars) - Helpers for assigning a person or place to a post, which attempt to locate an existing one, or create a new one if needed

---
+++ class-taxonomy-meta.php
@@ -0,0 +1,156 @@
+<?php
+
+/**
+ * Helpers for working with Taxonomy Meta.
+ * Makes it a lot easier to add meta fields, and no have to manually handle them
+ * on multiple UIs, saving, adding to tables, etc.
+ *
+ * Primarily, you want to use Taxonomy_Meta::add();
+ */
+
+class Taxonomy_Meta {
+	/**
+	 * Main interface to adding taxmeta. Call this with appropriate args.
+	 * @param String $tax The taxonomy to add this meta field to.
+	 * @param Array $args Details on how to set up this meta.
+	 *   - key:   The key for storing/accessing this meta field.
+	 *   - value: The current value of this meta.
+	 *   - label: User-facing label to show next to the input field.
+	 *   - type:  What sort of data is it? [ text ].
+	 *   - help:  A user-facing help string to display.
+	 *   - table: Boolean indicating if this should be included in the term listing table.
+	 */
+	static function add( $tax, $args ) {
+		if ( ! $tax || ! taxonomy_exists( $tax ) ) {
+			return false;
+		}
+
+		$defaults = array(
+			'value' => '',
+			'type'  => 'text',
+			'table' => true,
+		);
+		$args = wp_parse_args( $args, $defaults );
+
+		// Add fields to new/edit screens
+		add_action( $tax . '_add_form_fields', function() use ( $tax, $args ) {
+			Taxonomy_Meta::add_ui( $tax, $args );
+		}, 10, 2 );
+		add_action( 'created_' . $tax, function( $term_id, $tt_id ) use ( $tax, $args ) {
+			Taxonomy_Meta::add_handler( $term_id, $tt_id, $tax, $args );
+		}, 10, 2 );
+		add_action( $tax . '_edit_form_fields', function( $term, $taxonomy ) use ( $tax, $args ) {
+			Taxonomy_Meta::edit_ui( $term, $taxonomy, $tax, $args );
+		}, 10, 2 );
+		add_action( 'edited_' . $tax, function( $term_id, $tt_id ) use ( $tax, $args ) {
+			Taxonomy_Meta::edit_handler( $term_id, $tt_id, $tax, $args );
+		}, 10, 2 );
+
+		// Optionally include meta in the term listing table
+		if ( $args['table'] ) {
+			add_filter( 'manage_edit-' . $tax . '_columns', function( $columns ) use ( $tax, $args ) {
+				return Taxonomy_Meta::table_column( $columns, $tax, $args );
+			}, 10, 1 );
+			add_filter( 'manage_' . $tax . '_custom_column', function( $content, $column_name, $term_id ) use ( $tax, $args ) {
+				return Taxonomy_Meta::table_column_data( $content, $column_name, $term_id, $tax, $args );
+			}, 10, 3 );
+		}
+	}
+
+	/**
+	 * Handles injecting the required UI for adding new term.
+	 */
+	static function add_ui( $tax, $args ) {
+		Taxonomy_Meta::ui( 'add', $tax, $args );
+	}
+
+	/**
+	 * Handle the data received when saving new terms.
+	 */
+	static function add_handler( $term_id, $tt_id, $tax, $args ) {
+		if ( isset( $_POST[ $tax . '-' . $args['key'] ] ) && '' !== $_POST[ $tax . '-' . $args['key'] ] ){
+			$value = sanitize_text_field( $_POST[ $tax . '-' . $args['key'] ] );
+			add_term_meta( $term_id, $tax . '-' . $args['key'], $value, true );
+		}
+	}
+
+	/**
+	 * Handles injecting the required UI for editing existing terms.
+	 */
+	static function edit_ui( $term, $taxonomy, $tax, $args ) {
+		$args['value'] = get_term_meta( $term->term_id, $tax . '-' . $args['key'], true );
+		Taxonomy_Meta::ui( 'edit', $tax, $args );
+	}
+
+	/**
+	 * Handle the data received when saving existing terms.
+	 */
+	static function edit_handler( $term_id, $tt_id, $tax, $args ) {
+		if ( isset( $_POST[ $tax . '-' . $args['key'] ] ) && '' !== $_POST[ $tax . '-' . $args['key'] ] ){
+			$value = sanitize_text_field( $_POST[ $tax . '-' . $args['key'] ] );
+			update_term_meta( $term_id, $tax . '-' . $args['key'], $value );
+		}
+	}
+
+	/**
+	 * Create consistent UI for save/edit screens.
+	 * @param String $screen Which screen is this for [ add | edit ]
+	 */
+	static function ui( $screen = 'add', $tax, $args ) {
+		$id = esc_attr( $tax . '-' . $args['key'] );
+
+		if ( 'add' == $screen ) {
+			switch ( $args['type'] ) {
+			case 'text':
+			default:
+				?><div class="form-field term-group">
+						<label for="<?php echo $id;  ?>"><?php esc_html_e( $args['label'] ); ?></label>
+						<input type="text" name="<?php echo $id;  ?>" id="<?php echo $id;  ?>" value="<?php esc_attr_e( $args['value'] ); ?>" />
+						<?php if ( ! empty( $args['help'] ) ) : ?><p><?php esc_html_e( $args['help'] ); ?></p><?php endif; ?>
+				</div><?php
+				break;
+			}
+		} else { // screen == edit
+			switch ( $args['type'] ) {
+			case 'text':
+			default:
+				?><tr class="form-field">
+					<th scope="row">
+						<label for="<?php echo $id;  ?>"><?php esc_html_e( $args['label'] ); ?></label>
+					</th>
+					<td>
+						<input type="text" name="<?php echo $id;  ?>" id="<?php echo $id;  ?>" value="<?php esc_attr_e( $args['value'] ); ?>" />
+						<?php if ( ! empty( $args['help'] ) ) : ?><p class="description"><?php esc_html_e( $args['help'] ); ?></p><?php endif; ?>
+					</td>
+				</tr><?php
+				break;
+			}
+		}
+	}
+
+	/**
+	 * Add this meta into the term listing table (optional).
+	 */
+	static function table_column( $columns, $tax, $args ) {
+		$columns[ $args['key'] ] = $args['label'];
+		return $columns;
+	}
+
+	/**
+	 * Add this meta into the term listing table (optional).
+	 */
+	static function table_column_data( $content, $column_name, $term_id, $tax, $args ) {
+		if ( $args['key'] !== $column_name ) {
+			return $content;
+		}
+
+		$term_id = absint( $term_id );
+		$val = get_term_meta( $term_id, $tax . '-' . $args['key'], true );
+
+		if ( !empty( $val ) ){
+			$content .= esc_attr( $val );
+		}
+
+		return $content;
+	}
+}
---
+++ people-places.php
@@ -0,0 +1,395 @@
+<?php
+/*
+Plugin Name: People & Places
+Description: Add taxonomies for keeping track of people and locations, across different post types and data sources. Built primarily to work with Keyring Social Importers.
+Version: 1.0
+*/
+
+require_once dirname( __FILE__ ) . '/class-taxonomy-meta.php';
+
+class People_Places {
+	function __construct() {
+		add_action( 'init', array( $this, 'register_people_taxonomy' ) );
+		add_action( 'init', array( $this, 'register_places_taxonomy' ) );
+
+		add_action( 'restrict_manage_posts', array( $this, 'post_list_filter_people' ) );
+		add_action( 'restrict_manage_posts', array( $this, 'post_list_filter_places' ) );
+	}
+
+	/**
+	 * Since we're introducing new taxonomies, and we're likely to end up with a lot
+	 * of posts using them, let's make it a little easier to filter the admin to view.
+	 */
+	function post_list_filter_people() {
+		$terms = get_terms( 'people' );
+		if ( count( $terms ) ) :
+		?><select name="people" id="people">
+		<option value=""><?php esc_html_e( 'All People' ); ?></option>
+		<?php foreach ( $terms as $term ): ?>
+		<option<?php selected( isset( $_REQUEST['people'] ) && $_REQUEST['people'] == $term->slug ); ?> value="<?php echo esc_attr( $term->slug ); ?>"><?php echo esc_html( $term->name ); ?></option>
+		<?php endforeach; ?>
+		</select><?php
+		endif;
+	}
+
+	function post_list_filter_places() {
+		$terms = get_terms( 'places' );
+		if ( count( $terms ) ) :
+		?><select name="places" id="places">
+		<option value=""><?php esc_html_e( 'All Places' ); ?></option>
+		<?php foreach ( $terms as $term ): ?>
+		<option<?php selected( isset( $_REQUEST['places'] ) && $_REQUEST['places'] == $term->slug ); ?> value="<?php echo esc_attr( $term->slug ); ?>"><?php echo esc_html( $term->name ); ?></option>
+		<?php endforeach; ?>
+		</select><?php
+		endif;
+	}
+
+	/**
+	 * Register the People taxonomy, which is used to tie accounts together
+	 */
+	function register_people_taxonomy() {
+		$labels = [
+		'name'              => _x( 'People', 'taxonomy general name' ),
+		'singular_name'     => _x( 'Person', 'taxonomy singular name' ),
+		'search_items'      => __( 'Search People' ),
+		'all_items'         => __( 'All People' ),
+		'edit_item'         => __( 'Edit Person' ),
+		'update_item'       => __( 'Update Person' ),
+		'add_new_item'      => __( 'Add New Person' ),
+		'new_item_name'     => __( 'New Person' ),
+		'menu_name'         => __( 'People' ),
+		];
+		$args = [
+		'hierarchical'      => false,
+		'labels'            => $labels,
+		'show_ui'           => true,
+		'show_admin_column' => true,
+		'query_var'         => true,
+		'rewrite'           => [ 'slug' => 'people' ],
+		];
+		register_taxonomy( 'people', ['post'], $args );
+
+		$this->register_people_meta();
+
+		// Clean up columns a bit
+		add_filter( 'manage_edit-people_columns', function( $columns ) {
+			// Remove description, which could be long here.
+			unset( $columns['description'] );
+
+			// Shuffle "count" to the end of the table.
+			if ( ! empty( $columns['posts'] ) ) {
+				$posts = $columns['posts'];
+				unset( $columns['posts'] );
+				$columns['posts'] = $posts;
+			}
+
+			return $columns;
+		}, 10, 1 );
+	}
+
+	/**
+	 * Register associated meta for the People taxonomy
+	 */
+	function register_people_meta() {
+		Taxonomy_Meta::add( 'people', array(
+			'key'   => 'email',
+			'label' => __( 'Email' ),
+			'type'  => 'text',
+			'help'  => __( "This person's main email geo_longitude (also used to show a Gravatar)." ),
+			'table' => false,
+		) );
+		Taxonomy_Meta::add( 'people', array(
+			'key'   => 'url',
+			'label' => __( 'Web Address' ),
+			'type'  => 'text',
+			'help'  => __( "If they have one, what is this person's website/URL?" ),
+			'table' => false,
+		) );
+		Taxonomy_Meta::add( 'people', array(
+			'key'   => 'twitter',
+			'label' => __( 'Twitter screen name' ),
+			'type'  => 'text',
+			'help'  => __( "This person's Twitter screen/user name (without the '@')." ),
+			'table' => true,
+		) );
+		Taxonomy_Meta::add( 'people', array(
+			'key'   => 'instagram',
+			'label' => __( 'Instagram username' ),
+			'type'  => 'text',
+			'help'  => __( "This person's Instagram username." ),
+			'table' => true,
+		) );
+		Taxonomy_Meta::add( 'people', array(
+			'key'   => 'foursquare',
+			'label' => __( 'Foursquare id' ),
+			'type'  => 'text',
+			'help'  => __( "This person's Foursquare id." ),
+			'table' => false,
+		) );
+	}
+
+	/**
+	 * Get an image tag for the Gravatar of a user, based on their email address.
+	 * @param Int $term_id The id for this person's term entry
+	 * @param Int $size Pixel size of the image.
+	 * @return String Image tag for their Gravatar
+	 */
+	static function get_gravatar( $term_id, $size = 80 ) {
+		if ( $user = get_term_meta( $term_id, 'people-email', true ) ) {
+			$user = get_avatar( $user, $size );
+		}
+		return $user;
+	}
+
+	/**
+	 * Get the email address for someone, based on their term_id
+	 * @param  Int $term_id The id for this person's term entry
+	 * @return String Their email address
+	 */
+	static function get_email( $term_id ) {
+		return get_term_meta( $term_id, 'people-email', true );
+	}
+
+	/**
+	 * Get the URL for someone, based on their term_id
+	 * @param  Int $term_id The id for this person's term entry
+	 * @return String Their URL
+	 */
+	static function get_url( $term_id ) {
+		return get_term_meta( $term_id, 'people-url', true );
+	}
+
+	/**
+	 * Get the full URL to the Twitter profile page for someone, based on their term_id
+	 * @param  Int $term_id The id for this person's term entry
+	 * @return String URL to their Twitter profile, or empty string if none.
+	 */
+	static function get_twitter_url( $term_id ) {
+		if ( $user = get_term_meta( $term_id, 'people-twitter', true ) ) {
+			$user = 'https://twitter.com/' . $user;
+		}
+		return $user;
+	}
+
+	/**
+	 * Get the full URL to the Instagram profile page for someone, based on their term_id
+	 * @param  Int $term_id The id for this person's term entry
+	 * @return String URL to their Instagram profile, or empty string if none.
+	 */
+	static function get_instagram_url( $term_id ) {
+		if ( $user = get_term_meta( $term_id, 'people-instagram', true ) ) {
+			$user = 'https://instagram.com/' . $user;
+		}
+		return $user;
+	}
+
+	/**
+	 * Get the full URL to the Foursquare profile page for someone, based on their term_id
+	 * @param  Int $term_id The id for this person's term entry
+	 * @return String URL to their Foursquare profile, or empty string if none.
+	 */
+	static function get_foursquare_url( $term_id ) {
+		if ( $user = get_term_meta( $term_id, 'people-foursquare', true ) ) {
+			$user = 'https://foursquare.com/user/' . $user; // have to use this format because we don't always have username
+		}
+		return $user;
+	}
+
+	/**
+	 * Register the Places taxonomy, which allows us to tie geo data together under
+	 * a single location entry.
+	 */
+	function register_places_taxonomy() {
+		$labels = [
+		'name'              => _x( 'Places', 'taxonomy general name' ),
+		'singular_name'     => _x( 'Place', 'taxonomy singular name' ),
+		'search_items'      => __( 'Search Places' ),
+		'all_items'         => __( 'All Places' ),
+		'edit_item'         => __( 'Edit Place' ),
+		'update_item'       => __( 'Update Place' ),
+		'add_new_item'      => __( 'Add New Place' ),
+		'new_item_name'     => __( 'New Place' ),
+		'menu_name'         => __( 'Places' ),
+		];
+		$args = [
+		'hierarchical'      => false,
+		'labels'            => $labels,
+		'show_ui'           => true,
+		'show_admin_column' => true,
+		'query_var'         => true,
+		'rewrite'           => [ 'slug' => 'places' ],
+		];
+		register_taxonomy( 'places', ['post'], $args );
+
+		$this->register_places_meta();
+
+		// Clean up columns a bit
+		add_filter( 'manage_edit-places_columns', function( $columns ) {
+			// Remove description, which could be long here.
+			unset( $columns['description'] );
+
+			// Shuffle "count" to the end of the table.
+			if ( ! empty( $columns['posts'] ) ) {
+				$posts = $columns['posts'];
+				unset( $columns['posts'] );
+				$columns['posts'] = $posts;
+			}
+
+			return $columns;
+		}, 10, 1 );
+	}
+
+	/**
+	 * Register meta associated with locations/places
+	 */
+	function register_places_meta() {
+		Taxonomy_Meta::add( 'places', array(
+			'key'   => 'geo_latitude',
+			'label' => __( 'Latitude' ),
+			'type'  => 'text',
+			'help'  => __( "The latitude (in decimal notation) for this location." ),
+			'table' => false,
+		) );
+		Taxonomy_Meta::add( 'places', array(
+			'key'   => 'geo_longitude',
+			'label' => __( 'Longitude' ),
+			'type'  => 'text',
+			'help'  => __( "The longitude (in decimal notation) for this location." ),
+			'table' => false,
+		) );
+		Taxonomy_Meta::add( 'places', array(
+			'key'   => 'address',
+			'label' => __( 'Street Address' ),
+			'type'  => 'text',
+			'help'  => __( "Full street address, including city, state etc." ),
+			'table' => false,
+		) );
+		Taxonomy_Meta::add( 'places', array(
+			'key'   => 'foursquare',
+			'label' => __( 'Foursquare Venue id' ),
+			'type'  => 'text',
+			'help'  => __( "Unique identifier from Foursquare (md5-looking hash)." ),
+			'table' => false,
+		) );
+		Taxonomy_Meta::add( 'places', array(
+			'key'   => 'twitter',
+			'label' => __( 'Twitter Location id' ),
+			'type'  => 'text',
+			'help'  => __( "Unique identifier from Twitter, for this location." ),
+			'table' => false,
+		) );
+	}
+
+	/**
+	 * Helper to handle association a person to a post. It will automatically
+	 * create a term entry for the person if required, or use an existing one
+	 * based on matching the id supplied.
+	 * @param String $meta The key for the meta item to match against
+	 * @param Mixed $value The value for the key (id/username/etc)
+	 * @param Array $data Additional data used if we need to create the term.
+	 *                    Should contain a 'name' at least.
+	 * @param Int $post_id The post id to associate this entry with
+	 */
+	 static function add_person_to_post( $meta, $value, $data, $post_id ) {
+ 		// Resolve to existing entry?
+ 		$existing = get_terms( array(
+ 			'taxonomy'   => 'people',
+ 			'hide_empty' => false,
+ 			'meta_query' => array(
+ 				array(
+ 					'key'     => 'people-' . $meta,
+ 					'value'   => $value,
+ 					'compare' => '=',
+ 				)
+ 			)
+ 		) );
+ 		if ( $existing && ! is_wp_error( $existing ) ) {
+ 			$existing = $existing[0]->term_id;
+ 		} else {
+ 			// Create new entry to attach this data to
+ 			// @todo Handle duplicate names when we find people on different networks (auto-merge?)
+ 			$existing = wp_insert_term( $data['name'], 'people' );
+ 			if ( $existing && ! is_wp_error( $existing ) ) {
+ 				$existing = $existing['term_id'];
+
+ 				// Store the id we used as term meta, so that we don't recreate later
+ 				add_term_meta( $existing, 'people-' . $meta, $value );
+ 			}
+ 		}
+
+ 		if ( is_wp_error( $existing ) ) {
+ 			return false;
+ 		}
+
+ 		return wp_set_object_terms( $post_id, array( intval( $existing ) ),'people', true );
+ 	}
+
+	static function add_place_to_post( $meta, $value, $data, $post_id ) {
+		$existing = false;
+		// Resolve to existing entry?
+		$found = get_terms( array(
+			'taxonomy'   => 'places',
+			'hide_empty' => false,
+			'meta_query' => array(
+				array(
+					'key'     => 'places-' . $meta,
+					'value'   => $value,
+					'compare' => '=',
+				)
+			)
+		) );
+		if ( $found && ! is_wp_error( $found ) ) {
+			$existing = $found[0]->term_id;
+		} else if ( ! empty( $data['geo_latitude'] ) && ! empty( $data['geo_longitude'] ) ) {
+			// Match if there's somewhere with the same co-ordinates
+			$found = get_terms( array(
+				'taxonomy'   => 'places',
+				'hide_empty' => false,
+				'meta_query' => array(
+					'relation' => 'AND',
+					array(
+						'key'     => 'places-geo_latitude',
+						'value'   => $data['geo_latitude'],
+						'compare' => '=',
+					),
+					array(
+						'key'     => 'places-geo_longitude',
+						'value'   => $data['geo_longitude'],
+						'compare' => '=',
+					)
+				)
+			) );
+			if ( $found && ! is_wp_error( $found ) ) {
+				$existing = $found[0]->term_id;
+			}
+		}
+
+		// Create new entry to attach this data to if we haven't found one
+		if ( ! $existing ) {
+			// @todo handle duplicate names of places (e.g. Costco)
+			// We don't match very well since coordinates are likely to be different, even for the same place.
+			$inserted = wp_insert_term( $data['name'], 'places' );
+			if ( $inserted && ! is_wp_error( $inserted ) ) {
+				$existing = $inserted['term_id'];
+			}
+		}
+
+		// Store the id we used as term meta, so that we don't recreate later
+		add_term_meta( $existing, 'places-' . $meta, $value, true );
+
+		// Now store additional information if we've got it
+		if ( ! empty( $data['address'] ) ) {
+			add_term_meta( $existing, 'places-address', $data['address'], true );
+		}
+		if ( ! empty( $data['geo_latitude'] ) ) {
+			add_term_meta( $existing, 'places-geo_latitude', $data['geo_latitude'], true );
+		}
+		if ( ! empty( $data['geo_longitude'] ) ) {
+			add_term_meta( $existing, 'places-geo_longitude', $data['geo_longitude'], true );
+		}
+
+		return wp_set_object_terms( $post_id, array( intval( $existing ) ), 'places' );
+	}
+}
+
+new People_Places();
